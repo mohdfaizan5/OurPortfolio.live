13fb0f509e62ef590bc394808bd61797ec349523
Mohammed Faizan <mohdfaizan.zprb2@gmail.com>
2024-02-11 23:59:28 +0530

    Deleted stuff and working basics

--- src/components/Design2.jsx
+++ src/components/Design2.jsx
@@ -11,7 +11,7 @@ const Design2 = () => {
   userData = getUserDetails(username)
 
   return (
-    <div className="bg-blue-100 h-screen flex flex-col items-center">
+    <div className="py-10 gap-2 bg-blue-100 h-screen flex flex-col items-center">
       <h1 className="text-3xl">Hi {userData.name}</h1>
       <img src={userData.avatar_url} className="w-20 h-20 rounded-full" />
       <h3 className="text-balance text-center text-sm">{userData.bio}</h3>
--- src/constants.js
+++
@@ -1,1 +0,0 @@
-export const DB_NAME = "OurPortfolio_production"
